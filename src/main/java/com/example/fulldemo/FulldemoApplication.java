ca3cd9874dc93412f539e4c7346eb88f366ce14b
unknown <haris444.hv@gmail.com>
2025-02-18 22:23:44 +0200

    Version 1

---
+++ src/main/java/com/example/fulldemo/FulldemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.fulldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulldemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FulldemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fulldemo/FulldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fulldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
